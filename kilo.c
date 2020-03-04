6dd5a0f4b05950b3901f974f5982367a0de68094
Aman Pandey <amanp.it@sbjit.edu.in>
2020-03-04 22:13:55 +0530

    Moving the curser to right and left from begining and end of the line respectively

--- kilo.c
+++ kilo.c
@@ -35,8 +35,10 @@ enum editorKey {
 /*** data ***/
 
 typedef struct erow {
+  int rsize;
   int size;
   char *chars;
+  char *render;
 } erow;
 
 struct editorConfig {
@@ -174,6 +176,8 @@ void editorAppendRow(char *s, size_t len) {
   E.row[at].chars = malloc(len + 1);
   memcpy(E.row[at].chars, s, len);
   E.row[at].chars[len] = '\0';
+  E.row[at].rsize = 0;
+  E.row[at].render = NULL;
   E.numrows++;
 }
 
@@ -299,11 +303,17 @@ void editorMoveCursor(int key) {
     case ARROW_LEFT:
       if (E.cx != 0) {
         E.cx--;
+      }else if(E.cy > 0){
+        E.cy--;
+        E.cx = E.row[E.cy].size;
       }
       break;
     case ARROW_RIGHT:
       if (row && E.cx < row->size) {
         E.cx++;
+      }else if(row && E.cx == row->size){
+        E.cy++;
+        E.cx = 0;
       }
       break;
     case ARROW_UP:
