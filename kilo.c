e427e80a91626d3c2a43ba81cad58061a6ebaa0f
Aman Pandey <nameamanpandey1525@gmail.com>
2020-03-15 20:05:32 +0530

    backspace and del key

--- kilo.c
+++ kilo.c
@@ -241,6 +241,15 @@ void editorRowInsertChar(erow *row, int at, int c){
   E.dirty++;
 }
 
+
+void editorRowDelChar(erow *row, int at){
+  if(at<0  || at > row->size) return;
+  memmove(&row->chars[at], &row->chars[at+1], row->size - at);
+  row->size--;
+  editorUpdateRow(row);
+  E.dirty++;
+} 
+
 /*** editor operations ***/
 
 void editorInsertChar(int c){
@@ -250,7 +259,14 @@ void editorInsertChar(int c){
   editorRowInsertChar(&E.row[E.cy], E.cx, c);
   E.cx++;
 }
-
+void editorDelChar(){
+  if(E.cy == E.numrows) return;
+  erow *row = &E.row[E.cy];
+  if(E.cx > 0){
+    editorRowDelChar(row,E.cx-1);
+    E.cx--;
+  }
+}
 /*** file i/o ***/
 
 char *editorRowsToString(int *buflen){
@@ -528,7 +544,9 @@ void editorProcessKeypress() {
     case BACKSPACE:
     case CTRL_KEY('h'):
     case DEL_KEY:
-      /*todo*/
+      if (c == DEL_KEY) editorMoveCursor(ARROW_RIGHT);
+      editorDelChar();
+      break;
     case PAGE_UP:
     case PAGE_DOWN:
       {
