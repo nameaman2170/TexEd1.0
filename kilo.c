3662adf4f34a3d47fde40bfbda40cfd754846b67
nameaman2170 <nameamanpandey1525@gmail.com>
2020-03-15 19:23:11 +0530

    alert for unsave dchangfes

--- kilo.c
+++ kilo.c
@@ -19,6 +19,7 @@
 
 #define KILO_VERSION "0.0.1"
 #define KILO_TAB_STOP 8
+#define KILO_QUIT_TIMES 3
 
 #define CTRL_KEY(k) ((k) & 0x1f)
 
@@ -493,12 +494,19 @@ void editorMoveCursor(int key) {
 }
 
 void editorProcessKeypress() {
+  static int quit_times = KILO_QUIT_TIMES;
+
   int c = editorReadKey();
   switch (c) {
     case '\r':
       /*todo*/
       break;
     case CTRL_KEY('q'):
+      if(E.dirty && quit_times > 0){
+        editorSetStatusMessage("Warning!!! File has unsaved changes.""Press CTRL-Q %d more times to quit.", quit_times);
+        quit_times--;
+        return;
+      }
       write(STDOUT_FILENO, "\x1b[2J", 4);
       write(STDOUT_FILENO, "\x1b[H", 3);
       exit(0);
@@ -549,6 +557,7 @@ void editorProcessKeypress() {
       editorInsertChar(c);
       break;
   }
+  quit_times = KILO_QUIT_TIMES;
 }
 
 /*** init ***/
