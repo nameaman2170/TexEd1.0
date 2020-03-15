85dd92a0dead2c8c9c88f386155f9f8918a60072
Aman Pandey <amanp.it@sbjit.edu.in>
2020-03-15 08:04:13 +0530

    Save to disk and Error Handling

--- kilo.c
+++ kilo.c
@@ -4,6 +4,7 @@
 
 #include <ctype.h>
 #include <errno.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -244,6 +245,25 @@ void editorInsertChar(int c){
 
 /*** file i/o ***/
 
+char *editorRowsToString(int *buflen){
+  int totlen = 0;
+  int j;
+  for(j = 0; j < E.numrows; j++){
+    totlen += E.row[j].size + 1;
+  }
+  *buflen = totlen;
+
+  char *buf = malloc(totlen);
+  char *p =buf;
+  for(j = 0; j< E.numrows; j++){
+    memcpy(p, E.row[j].chars, E.row[j].size);
+    p += E.row[j].size;
+    *p = '\n';
+    p++;
+  }
+  return buf;
+}
+
 void editorOpen(char *filename) {
   free(E.filename);
   E.filename = strdup(filename);
@@ -261,6 +281,25 @@ void editorOpen(char *filename) {
   }
   free(line);
   fclose(fp);
+}
+
+void editorSave(){
+  if(E.filename == NULL) return;
+  int len;
+  char *buf = editorRowsToString(&len);
+
+  int fd = open(E.filename, O_RDWR | O_CREAT, 0644);
+  if(fd != -1){
+    if(ftruncate(fd, len) != -1){
+      if(write(fd, buf, len) == len){
+          close(fd);
+          free(buf);
+          return;
+      }
+    }
+    close(fd);
+  }
+  free(buf);
 }
 
 /*** append buffer ***/
@@ -450,6 +489,10 @@ void editorProcessKeypress() {
       write(STDOUT_FILENO, "\x1b[2J", 4);
       write(STDOUT_FILENO, "\x1b[H", 3);
       exit(0);
+      break;
+
+    case CTRL_KEY('s'):
+      editorSave();
       break;
 
     case HOME_KEY:
